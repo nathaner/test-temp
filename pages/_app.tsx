c9147f1f17e96a35621528a4630c08dfe096a9f5
Nathan <n.e.riviere@gmail.com>
2021-01-13 23:51:42 +0100

    Fix removal of NavBar and Footer from _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,13 @@
 import '../styles/globals.scss';
+import Footer from '../layouts/components/Footer';
+import NavBar from '../layouts/components/NavBar';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
