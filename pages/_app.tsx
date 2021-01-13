10773580bf4e4a97045b82070f3cba31192a1f6d
Nathan <n.e.riviere@gmail.com>
2021-01-13 23:45:22 +0100

    Edit _app.tsx and use yarn

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,34 +1,5 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Head from 'next/head';
-import '../styles/globals.scss'
-import BaseLayout from '../layouts/BaseLayout';
+import '../styles/globals.scss';
 
-export default function MyApp(props) {
-  const { Component, pageProps } = props;
-
-  React.useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
-    }
-  }, []);
-
-  return (
-    <React.Fragment>
-      <Head>
-        <title>My page</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-      </Head>
-      <BaseLayout>
-        <Component {...pageProps} />
-      </BaseLayout>
-    </React.Fragment>
-  );
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
-
-MyApp.propTypes = {
-  Component: PropTypes.elementType.isRequired,
-  pageProps: PropTypes.object.isRequired,
-};
