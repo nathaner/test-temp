3be020c77be853d2482f3b0f7463c0d653e99a38
Nathan <n.e.riviere@gmail.com>
2021-01-17 22:49:12 +0100

    Add response menu and remove yarn

--- layouts/components/NavBar.tsx
+++ layouts/components/NavBar.tsx
@@ -1,48 +1,89 @@
-import Link from 'next/link';
-import ButtonLink from '../../components/common/ButtonLink';
-import VaultinumLogo from './VaultinumLogo';
+import Link from "next/link";
+import { useState } from "react";
+import VaultinumLogo from "./VaultinumLogo";
+import classNames from "classnames"
 
 const MENU_ITEMS = [
-  {
-    name: 'You are',
-    path: '/you-are',
-  },
-  {
-    name: 'Solutions',
-    path: '/solutions',
-  },
-  {
-    name: 'About us',
-    path: '/about-us',
-  },
-  {
-    name: 'Contact',
-    path: '/contact',
-  },
+    {
+        name: "You are",
+        path: "/you-are",
+    },
+    {
+        name: "Solutions",
+        path: "/solutions",
+    },
+    {
+        name: "About us",
+        path: "/about-us",
+    },
+    {
+        name: "Contact",
+        path: "/contact",
+    },
 ];
 
 export default function NavBar() {
-  return (
-    <nav className="container">
-      <div>
-        <Link href="/">
-          <a>
-            <VaultinumLogo />
-          </a>
-        </Link>
-      </div>
-      <ul>
-        {MENU_ITEMS.map((item, index) => (
-          <li key={index}>
-            <Link href={item.path}>
-              <a>{item.name}</a>
+    const [open, setOpen] = useState(false);
+
+    const handleOpen = () => {
+        console.log("ok");
+
+        setOpen((open) => !open);
+    };
+
+    return (
+        <nav className="navbar container">
+            <Link href="/">
+                <a>
+                    <VaultinumLogo />
+                </a>
             </Link>
-          </li>
-        ))}
-      </ul>
-      <div>
-        <ButtonLink href="/">Sign in</ButtonLink>
-      </div>
-    </nav>
-  );
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                className="open"
+                onClick={handleOpen}
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                />
+            </svg>
+            <div className={classNames("menu", { open })}>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    className="close"
+                    onClick={handleOpen}
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M6 18L18 6M6 6l12 12"
+                    />
+                </svg>
+                <ul className="list-unstyled">
+                    {MENU_ITEMS.map((item, index) => (
+                        <li key={index}>
+                            <Link href={item.path}>
+                                <a>{item.name}</a>
+                            </Link>
+                        </li>
+                    ))}
+                    <li>
+                        <a href="" className="btn primary">
+                            Sign in
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
 }
